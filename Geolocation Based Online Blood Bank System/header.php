2091723cdf5ae01a6cc059738feaa8cdfb92d4b9
Biplob45 <mdbadiuzzamanbiplob@gmail.com>
2018-02-15 02:35:35 -0800

    Changed Title
    I changed my title.Because My Previous title is not matched my project
    name.

--- Geolocation Based Online Blood Bank System/header.php
+++ Geolocation Based Online Blood Bank System/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Geo-location Based Blood Bank</title>
+        <title>Geolocation Based Online Blood Bank System</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
